42dca5fe20dde6fd8ea6f7b6079db420b44f9702
surajan shrestha <surajansth1995@gmail.com>
2025-02-09 18:18:44 +0545

    updated boxes

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Gửi em",
+
+    background: "background.jpg",
+
+    min1: "1.jpg",
+    min2: "2.jpg",
+    min3: "4.jpg",
+    min4: "6.jpg",
+    min5: "11.jpg",
+    min6: "15.jpg",
+
+    max1: "1.jpg",
+    max2: "2.jpg",
+    max3: "4.jpg",
+    max4: "6.jpg",
+    max5: "10.jpg",
+    max6: "11.jpg",
+}
